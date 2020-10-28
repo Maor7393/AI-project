de138f7534fc066732fc92e1b2bff776f4884593
omriattal <omriattal@gmail.com>
2020-10-28 17:34:05 +0200

    fixed indenation

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -1,22 +1,29 @@
+import graph
+import vertex
+
+if __name__ == "__main__":
+	print("hi")
+	graph = graph.Graph();
+
 def generateGraph(file_name):
-    graph = Graph()
-    file = open(file_name)
-    lines = file.readlines()
-    vertices = {}
-    for line in lines:
-    	type = line[0]
-    	line = line.split()
-    	if type == 'V': 
-        	name = line[1]
-        	number_of_people = line[2]
-        	v = vertex(name, number_of_people)
-        	vertices[v.name] = v
-        	graph.add_vertex(v)          
-        elif type == 'E':
-            source_name = line[1]
-            target_name = line[2]
-            edge_weight = line[3] 
-            v1 = vertices[source_name]
-            v2 = vertices[target_name]
-            graph.add_edge(v1, v2, edge_weight)
-    pass
+	graph = graph.Graph()
+	file = open(file_name)
+	lines = file.readlines()
+	vertices = {}
+	for line in lines:
+		type = line[0]
+		line = line.split(' ')
+		if type == 'V':
+			name = line[1]
+			number_of_people = line[2]
+			v = vertex(name, number_of_people)
+			vertices[v.name] = v
+			graph.add_vertex(v)          
+		elif type == 'E':
+			source_name = line[1]
+			target_name = line[2]
+			edge_weight = line[3] 
+			v1 = vertices[source_name]
+			v2 = vertices[target_name]
+			graph.add_edge(v1, v2, edge_weight)
+	
--- Assignment 1/graph.py
+++ Assignment 1/graph.py
@@ -7,6 +7,7 @@ class Graph(object):
             graph_dict = {}
         self.__graph_dict = graph_dict
 
+
     def vertices(self):
         return list(self.__graph_dict.keys())
 
@@ -47,45 +48,4 @@ class Graph(object):
         for edge in self.__generate_edges():
             res += str(edge) + ", "
         return res
-
-
-if __name__ == "__main__":
-
-    g = { "a" : ["d"],
-          "b" : ["c"],
-          "c" : ["b", "c", "d", "e"],
-          "d" : ["a", "c"],
-          "e" : ["c"],
-          "f" : []
-        }
-
-
-    graph = Graph(g)
-
-    print("Vertices of graph:")
-    print(graph.vertices())
-
-    print("Edges of graph:")
-    print(graph.edges())
-
-    print("Add vertex:")
-    graph.add_vertex("z")
-
-    print("Vertices of graph:")
-    print(graph.vertices())
- 
-    print("Add an edge:")
-    graph.add_edge({"a","z"})
-    
-    print("Vertices of graph:")
-    print(graph.vertices())
-
-    print("Edges of graph:")
-    print(graph.edges())
-
-    print('Adding an edge {"x","y"} with new vertices:')
-    graph.add_edge({"x","y"})
-    print("Vertices of graph:")
-    print(graph.vertices())
-    print("Edges of graph:")
-    print(graph.edges())
+		
