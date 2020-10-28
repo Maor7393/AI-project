aff159fbcbdea58d20a44d79f1a96178640847c6
omriattal <omriattal@gmail.com>
2020-10-28 21:39:44 +0200

    implemented generate graph method

--- Assignment 1/graph.py
+++ Assignment 1/graph.py
@@ -1,12 +1,18 @@
+import vertex as v
+
+
 class Graph(object):
 
     def __init__(self, graph_dict=None):
         if graph_dict is None:
             graph_dict = {}
-        self.__graph_dict = graph_dict
+        self.graph_dict = graph_dict
 
     def vertices(self):
-        return list(self.__graph_dict.keys())
+        return list(self.graph_dict.keys())
+
+    def edges(self):
+        return self.generate_edges()
 
     def vertices_names(self):
         name_list = []
@@ -14,36 +20,33 @@ class Graph(object):
             name_list.append(vertex.name)
         return name_list
 
-    def edges(self):
-        return self.__generate_edges()
-
     def vertex_exists(self, vertex):
-        return vertex.name in self.vertices_names();
+        return vertex.name in self.vertices_names()
 
     def add_vertex(self, vertex):
         if not self.vertex_exists(vertex):
-            self.__graph_dict[vertex] = []
+            self.graph_dict[vertex] = []
 
     def add_edge(self, vertex1, vertex2, weight):
         if self.vertex_exists(vertex1):
-            self.__graph_dict[vertex1].append((vertex2, weight))
+            self.graph_dict[vertex1].append((vertex2, weight))
         else:
-            self.__graph_dict[vertex1] = [(vertex2, weight)]
+            self.graph_dict[vertex1] = [(vertex2, weight)]
 
-    def __generate_edges(self):
+    def generate_edges(self):
         edges = []
-        for vertex in self.__graph_dict:
-            for neighbor_tuple in self.__graph_dict[vertex]:
+        for vertex in self.graph_dict:
+            for neighbor_tuple in self.graph_dict[vertex]:
                 edges.append((vertex, neighbor_tuple[0], neighbor_tuple[1]))
         return edges
 
     def __str__(self):
         res = "vertices: "
-        for k in self.__graph_dict:
-            res += str(k) + " "
+        for k in self.graph_dict:
+            res += str(k) + ", "
         res += "\nedges: "
-        for edge in self.__generate_edges():
-            res += str(edge) + ", "
+        for edge in self.generate_edges():
+            res += "("+edge[0].name + ", " + edge[1].name + ", " + str(edge[2]) + "), "
         return res
 
 
@@ -51,21 +54,21 @@ def generate_graph(file_name):
     output_graph = Graph()
     file = open(file_name)
     lines = file.readlines()
-    vertices = {}
+    name_vertices_dict = {}
     for line in lines:
         element_type = line[0]
         line = line.split(' ')
         if element_type == 'V':
             name = line[1]
-            number_of_people = line[2]
-            vertex = g.vertex(name, number_of_people)
-            vertices[vertex.name] = vertex
-            output_graph.add_vertex(v)
+            number_of_people = int(line[2])
+            vertex = v.Vertex(name, number_of_people)
+            name_vertices_dict[vertex.name] = vertex
+            output_graph.add_vertex(vertex)
         elif element_type == 'E':
             source_name = line[1]
             target_name = line[2]
-            edge_weight = line[3]
-            v1 = vertices[source_name]
-            v2 = vertices[target_name]
+            edge_weight = int(line[3])
+            v1 = name_vertices_dict[source_name]
+            v2 = name_vertices_dict[target_name]
             output_graph.add_edge(v1, v2, edge_weight)
     return output_graph
--- Assignment 1/vertex.py
+++ Assignment 1/vertex.py
@@ -1,7 +1,7 @@
-class Vertex (object) :
-    def __init__(self,name,number_of_people):
+class Vertex (object):
+    def __init__(self, name, number_of_people):
         self.name = name
         self.number_of_people = number_of_people
 
     def __str__(self):
-        return "name: "+self.name + "num of people: "+ str(self.number_of_people)
+        return "["+self.name + ", " + str(self.number_of_people)+"]"
