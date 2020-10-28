4be5a2bba7199b76977bae30009ab440a2c5542d
danya <danyarden14@gmail.com>
2020-10-28 21:11:20 +0200

    first commit in Pycharm

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -1,33 +1,5 @@
 import graph as g
 
-import vertex as v
-
 if __name__ == "__main__":
-	print("hi")
-	graph = g.Graph();
-	vertex = v.Vertex("omri",7);
-	print(vertex)
-
-
-def generateGraph(file_name):
-	graph = g.Graph()
-	file = open(file_name)
-	lines = file.readlines()
-	vertices = {}
-	for line in lines:
-		type = line[0]
-		line = line.split(' ')
-		if type == 'V':
-			name = line[1]
-			number_of_people = line[2]
-			vertex = g.vertex(name, number_of_people)
-			vertices[vertex.name] = vertex
-			graph.add_vertex(v)          
-		elif type == 'E':
-			source_name = line[1]
-			target_name = line[2]
-			edge_weight = line[3] 
-			v1 = vertices[source_name]
-			v2 = vertices[target_name]
-			graph.add_edge(v1, v2, edge_weight)
-	
+    inputGraph = g.generate_graph("graph.txt")
+    print(inputGraph)
--- Assignment 1/graph.py
+++ Assignment 1/graph.py
@@ -1,9 +1,7 @@
-import vertex
-
 class Graph(object):
 
     def __init__(self, graph_dict=None):
-        if graph_dict == None:
+        if graph_dict is None:
             graph_dict = {}
         self.__graph_dict = graph_dict
 
@@ -12,31 +10,31 @@ class Graph(object):
 
     def vertices_names(self):
         name_list = []
-        for vertex in self.vertices(self):
+        for vertex in self.vertices():
             name_list.append(vertex.name)
         return name_list
 
     def edges(self):
         return self.__generate_edges()
 
-    def vertexExists(self,vertex):
+    def vertex_exists(self, vertex):
         return vertex.name in self.vertices_names();
 
     def add_vertex(self, vertex):
-        if not self.vertexExists(vertex):
+        if not self.vertex_exists(vertex):
             self.__graph_dict[vertex] = []
 
-    def add_edge(self, vertex1,vertex2,weight):
-        if  self.vertexExists(vertex1):
-            self.__graph_dict[vertex1].append((vertex2,weight))
+    def add_edge(self, vertex1, vertex2, weight):
+        if self.vertex_exists(vertex1):
+            self.__graph_dict[vertex1].append((vertex2, weight))
         else:
-            self.__graph_dict[vertex1] = [(vertex2,weight)]
+            self.__graph_dict[vertex1] = [(vertex2, weight)]
 
     def __generate_edges(self):
         edges = []
         for vertex in self.__graph_dict:
             for neighbor_tuple in self.__graph_dict[vertex]:
-                edges.append((vertex,neighbor_tuple[0],neighbor_tuple[1]))
+                edges.append((vertex, neighbor_tuple[0], neighbor_tuple[1]))
         return edges
 
     def __str__(self):
@@ -47,4 +45,27 @@ class Graph(object):
         for edge in self.__generate_edges():
             res += str(edge) + ", "
         return res
-		
+
+
+def generate_graph(file_name):
+    output_graph = Graph()
+    file = open(file_name)
+    lines = file.readlines()
+    vertices = {}
+    for line in lines:
+        element_type = line[0]
+        line = line.split(' ')
+        if element_type == 'V':
+            name = line[1]
+            number_of_people = line[2]
+            vertex = g.vertex(name, number_of_people)
+            vertices[vertex.name] = vertex
+            output_graph.add_vertex(v)
+        elif element_type == 'E':
+            source_name = line[1]
+            target_name = line[2]
+            edge_weight = line[3]
+            v1 = vertices[source_name]
+            v2 = vertices[target_name]
+            output_graph.add_edge(v1, v2, edge_weight)
+    return output_graph
