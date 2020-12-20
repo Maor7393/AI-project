849d899ad99dc20c6ed6dd9e4c9e9ec620fc8d1f
omriattal <omriattal@gmail.com>
2020-12-20 13:23:51 +0200

    changed printing of node.

--- Assignment 3/node.py
+++ Assignment 3/node.py
@@ -11,6 +11,14 @@ class Node:
         s = ""
         s += "Node Name: " + self.name + "\n"
         s += "Probability Table: " + "\n"
+        s += "-------------------------------"
+        for table_entry in self.table.table_dict.keys():
+            s += "P(" + self.name + "= True |  " + str(table_entry) + ")= " + str(self.table.table_dict.get(table_entry)) + "\n"
+            s += "P(" + self.name + "= False |  " + str(table_entry) + ")= " + str(1 - self.table.table_dict.get(table_entry)) + "\n"
+        s += "-------------------------------\n"
+        return s
+
+
         s += str(self.table)
 
         return s
